e50bc52184e79b8b2d0a58bdb97bb206c5d59671
pavithra147 <pavithra.jayavel@aspiresys.com>
2022-12-26 19:04:59 +0530

    template driven form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { User } from './user';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'AngularForms';
+  //two-way data binding
+  //from model to template form
+   //To create instance for constructor of class user (model)
+   userDetails = new User("Pavi","abc@gmail.com",8987349329);
 }
---
+++ src/app/user.ts
@@ -0,0 +1,7 @@
+export class User {    //model
+    constructor(
+        public name:string,
+        public email:string,
+        public  phoneno:number
+       ){}
+}
